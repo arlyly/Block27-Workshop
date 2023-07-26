46163d8e24b8495795261954a34af2c26f79e47e
arlyly <jeanalexandrealy@gmail.com>
2023-07-26 19:59:53 -0400

    Working on the Authenticate component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@
 import './App.css'
 import SignUpForm from './components/SignUpForm'
 import Authenticate from './components/Authenticate'
+import { useState } from 'react'
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  const [token, setToken] = useState(null);
   return (
     <div>
         <h2>Block 27 - Authentication</h2>
-        <Authenticate />
-        <SignUpForm />
+        <Authenticate token={token} setToken={setToken} />
+        <SignUpForm token={token} setToken={setToken} />
 
     </div>
  
-  )
+  );
 }
 
 export default App
