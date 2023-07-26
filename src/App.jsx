d10b8a353e0a46901416d9ed24b1e1024a8ceafc
Oghenetega Etareh <tega.etare@gmail.com>
2023-07-25 21:13:06 -0400

    Sign up Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+// import { useState } from 'react'
 import './App.css'
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
 
 function App() {
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <div>
+        <h2>Block 27 - Authentication</h2>
+        <Authenticate />
+        <SignUpForm />
+
+    </div>
+ 
   )
 }
 
